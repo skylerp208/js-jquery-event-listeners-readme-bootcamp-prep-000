3d65111779cc90ddfda5125de905c0caf7f20c53
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-08-29 02:51:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,6 +1,44 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+   }
+  )
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    img = tasty
+   }
+  )
+}
+
+function pressIt() {
+  $(document).on('keypress',
+  function(key) {
+    if (key.which == 71) {
+      alert ('You have pressed "g"')
+    }
+  }
+)
+}
+
+function submitIt() {
+  $('form').on('submit', function()
+  {
+    if ($('input:first').val() === 'type') {
+      alert("Your form is going to be submitted now.");
+      return;
+    } 
+  })
+}
+
 
 $(document).ready(function(){
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 // call functions here
 
