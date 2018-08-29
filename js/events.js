8d591ae667b61e53e55530d28b4dac1d579592bc
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-08-29 03:14:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,41 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+   }
+  )
+}
+function frameIt() {
+  $('img').on('load', function(){
+   document.getElementsByTagName('img')[0].classList.add('tasty')
+   }
+  )
+}
+
+function pressIt() {
+  $(document).on('keydown',
+  function(key) {
+    if (key.which == 71) {
+      alert ('You have pressed "g"')
+    }
+  }
+)
+}
+
+function submitIt() {
+  $('form').on('submit', function()
+  {
+      alert("Your form is going to be submitted now.");
+      return;
+  })
+}
+
 
 $(document).ready(function(){
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 // call functions here
 
